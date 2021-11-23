83adab0e35a0a370ff724c1477c11acc19194e81
vonLeebpl <vonleebpl@gmail.com>
2021-11-23 16:57:25 +0100

    cmu

--- PyTorch/SpeechSynthesis/FastPitch/scripts/download_cmudict.sh
+++ PyTorch/SpeechSynthesis/FastPitch/scripts/download_cmudict.sh
@@ -3,4 +3,4 @@
 set -e
 
 echo "Downloading cmudict-0.7b ..."
-wget https://github.com/Alexir/CMUdict/raw/master/cmudict-0.7b 
+wget https://github.com/Alexir/CMUdict/raw/master/cmudict-0.7b -qO ./DeepLearningExamples/PyTorch/SpeechSynthesis/FastPitch/cmudict/cmudict-0.7b
